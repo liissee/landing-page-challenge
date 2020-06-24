0d61286af90595009eaedf6175220103ecee770b
Linda <isell.linda@gmail.com>
2020-06-24 23:30:45 +0200

    Start on hero image and text

---
+++ script.js
@@ -0,0 +1,21 @@
+// // const navBarColor = () => {
+// //   const menuButton = document.getElementById("headerId")
+// //   menuButton.style.background = "hsla(229, 31%, 21%, 0.952)"
+// // }
+
+// const navBarColor = () => {
+//   const headerBackground = document.getElementById("headerId");
+//   headerBackground.classList.toggle("open");
+
+
+
+//   const svgText = document.getElementById("text");
+//   svgText.classList.toggle("text");
+
+// }
+
+// if (window.innerWidth < 760) {
+//   const navButton = document.getElementById("navButton")
+//   navButton.style = 'background-color: inherit; color: white; width: 80%; margin-top: 40px;';
+// }
+
